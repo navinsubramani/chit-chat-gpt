d24de60598f21e68e9cee08a3dbec211de1acc58
Navin Subramani <navinsubramaniam@gmail.com>
2023-04-30 10:16:56 -0400

    Simple Python Web App built on Quart that serves ChitChatGPT

---
+++ chitchatservice/openAI_utils.py
@@ -0,0 +1,116 @@
+import os
+import openai
+
+# Load the keys from a json file
+import json
+f = open('keys')
+data = json.load(f)
+ORGANIZATION_ID = data['ORG_KEY']
+API_KEY = data['API_KEY']
+
+openai.organization = ORGANIZATION_ID
+openai.api_key = API_KEY
+
+def create_system_message(you, them, onTopic):
+    return f"You should act as {you} and respond like how the character is and use the same accent always. Also have sarcasm and be witty in your tone and do not agree to others perspective, if it does not seem correct. You will be talking to {them}. This is a conversation between you both. Always respond with 1 or 2 sentances. Your topic for discussion is {onTopic}."
+
+
+def generate_chitchat(character1, character2, topic):
+    '''Generate Chit Chat using GPT'''
+    user1 = character1
+    user2 = character2
+    conversationCount = 11
+
+    user1_system = create_system_message(user1, user2, topic)
+    user2_system = create_system_message(user2, user1, topic)
+
+    user1_response = []
+    user2_response = ["Hi"]
+
+    isUser1 = True
+    for _ in range(conversationCount):
+        # user 1
+        if(isUser1):
+            prompts = []
+            for i, resp in enumerate(user2_response):
+                prompts.append({"role": "user", "content": resp})
+                if len(user1_response) > i:
+                    prompts.append({"role": "assistant", "content": user1_response[i]})
+            
+        else:
+            prompts = []
+            for i, resp in enumerate(user1_response):
+                prompts.append({"role": "user", "content": resp})
+                if len(user2_response)-1 > i:
+                    prompts.append({"role": "assistant", "content": user2_response[i+1]})
+
+        system_prompt = [{"role": "system", "content": user1_system} if isUser1 else {"role": "system", "content": user2_system}]
+        messages = system_prompt + prompts
+
+        completion = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        temperature=1,
+                        messages=messages)
+        
+        user1_response.append(completion.choices[0].message.content) if isUser1 else user2_response.append(completion.choices[0].message.content)
+        isUser1 = False if isUser1 else True
+    
+
+    response = []
+    for i, value in enumerate(user2_response):
+        response.append({"name": user2, "text": value})
+        response.append({"name": user1, "text": user1_response[i]})
+    
+    del response[0]
+    return response
+
+
+def stream_chitchat(info, topic):
+    '''Generate Chit Chat using GPT'''
+    user1 = info["character1"]
+    user2 = info["character2"]
+    isUser1 = info["isUser1"]
+    user1_response = info["user1_response"]
+    user2_response = info["user2_response"]
+
+    user1_system = create_system_message(user1, user2, topic)
+    user2_system = create_system_message(user2, user1, topic)
+
+    # user 1
+    if(isUser1):
+        prompts = []
+        for i, resp in enumerate(user2_response):
+            prompts.append({"role": "user", "content": resp})
+            if len(user1_response) > i:
+                prompts.append({"role": "assistant", "content": user1_response[i]})
+        
+    else:
+        prompts = []
+        for i, resp in enumerate(user1_response):
+            prompts.append({"role": "user", "content": resp})
+            if len(user2_response)-1 > i:
+                prompts.append({"role": "assistant", "content": user2_response[i+1]})
+
+    system_prompt = [{"role": "system", "content": user1_system} if isUser1 else {"role": "system", "content": user2_system}]
+    messages = system_prompt + prompts
+
+    completion = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    temperature=1,
+                    messages=messages)
+    
+    user1_response.append(completion.choices[0].message.content) if isUser1 else user2_response.append(completion.choices[0].message.content)
+    isUser1 = False if isUser1 else True
+
+    new_info = {
+        "character1" : user1,
+        "character2" : user2,
+        "isUser1" : isUser1,
+        "user1_response" : user1_response,
+        "user2_response" : user2_response
+    }
+
+    return {
+        "info" : new_info,
+        "result" : { "name": user2 if isUser1 else user1, "text": completion.choices[0].message.content }
+    }
---
+++ chitchatservice/server.py
@@ -0,0 +1,101 @@
+import json
+
+import quart
+import quart_cors
+from quart import request, stream_with_context, websocket, Quart
+
+import openAI_utils
+import asyncio
+
+app = quart_cors.cors(quart.Quart(__name__), allow_origin=["http://localhost:3000", "null"])
+
+@app.route("/chitchatgpt/generate", methods=["POST"])
+async def generate_chitchat():
+    '''Generate the ChitCHat Conversation based on the Given inputs'''
+    request_data = await request.get_data()
+    request_data = json.loads(request_data)
+    print(request_data)
+
+    response = openAI_utils.generate_chitchat(request_data["character1"], request_data["character2"], request_data["topic"])
+    return quart.Response(response=json.dumps(response), status=200, content_type="application/json")
+
+@app.websocket('/chitchatgpt/stream1')
+async def ws1():
+    try:
+        message = await websocket.receive()
+        request_data = json.loads(message)
+        print(request_data)
+
+        number_of_conversation = 11
+        info = {
+            "character1" : request_data["character1"],
+            "character2" : request_data["character2"],
+            "isUser1" : True,
+            "user1_response" : [],
+            "user2_response" : ["Hi"]
+        }
+        
+        for _ in range(number_of_conversation):
+            result = openAI_utils.stream_chitchat(info , request_data["topic"])
+            info = result["info"]
+            #print(result["result"])
+            await websocket.send(json.dumps(result["result"]))
+        await websocket.close(1000)
+
+    except asyncio.CancelledError:
+        # Handle disconnection here
+        print("Connection cancelled...")
+
+
+class WebSocketManager:
+    def __init__(self):
+        self.connections = set()
+
+    async def connect(self, websocket):
+        self.connections.add(websocket)
+        print("**********New Connection**************")
+
+    async def disconnect(self, websocket):
+        self.connections.remove(websocket)
+        print("**********Closing Connection************")
+
+    async def handle_chat(self, websocket):
+        message = await websocket.receive()
+        print("**********Recieved and Acting on data Connection*********")
+        request_data = json.loads(message)
+
+        number_of_conversation = 5
+        info = {
+            "character1": request_data["character1"],
+            "character2": request_data["character2"],
+            "isUser1": True,
+            "user1_response": [],
+            "user2_response": ["Hi"],
+        }
+
+        for _ in range(number_of_conversation):
+            result = openAI_utils.stream_chitchat(info, request_data["topic"])
+            info = result["info"]
+            await websocket.send(json.dumps(result["result"]))
+
+
+websocket_manager = WebSocketManager()
+
+@app.websocket('/chitchatgpt/stream')
+async def ws():
+    print("*********Websocket is called********")
+    await websocket_manager.connect(websocket)
+    try:
+        await websocket_manager.handle_chat(websocket)
+    except asyncio.CancelledError:
+        print("Connection cancelled...")
+    finally:
+        await websocket_manager.disconnect(websocket)
+
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5004)
+
+if __name__ == "__main__":
+    main()
